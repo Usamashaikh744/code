dac00176d4a139ad491cd4d342c9bfd797f5d629
Usama Shaikh <48800317+Usamashaikh744@users.noreply.github.com>
2019-06-20 17:49:27 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+let speed =90;
+
+if (speed > 60) {
+    console.log("YOU ARE OVERSPEEDING");
+} else {
+    console.log("NOT");
+}
+let islogin = "true";
+if (islogin) {
+    console.log("Welcome,you!")
+} else {
+    console.log("Please log in to continue")
+}
+let temp = 41;
+if (temp > 40){
+    alert("IMPORTANT ANNOUNCEMENT!!! UIT is closed tomorrow due to heat wave");
+}
+let account= 1000000;
+if (account < 100){
+    console.log("Congratulations! You are a millionaire")
+    } else {
+       console.log( "I'm sorry, you are not a millionaire");
+    }
+let carElectric = "true";
+if(carElectric =="false") {
+    console.log("made By Tesla")
+} else {
+    console.log("Not MAde By Tesla")
+}
+let userScore = 40;
+let highScore = 20;
+let result = userScore;
+let need = highScore - userScore;
+
+if(userScore > highScore) {
+    console.log("Congratulations!! Your new high score is 40") + console.log ("result")
+} else{
+    console.log("Try Again. You just need <the number of more points needed to beat the high score> to win!") + console.log ("need");
+}
+let number = 200 - 100;
+let evenNumber = number % 2 ;
+if (evenNumber == 0){
+    console.log("This is EVEN")
+} else{
+    console.log("This is ODD");
+}
+
